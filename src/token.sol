d5473054d20b51e0241156d39c25601ab76dc316
Rumena Staneva <rumenastaneva2106@gmail.com>
2026-01-15 16:01:52 +0200

    fixed broken mint logic supply

--- src/token.sol
+++ src/token.sol
@@ -8,12 +8,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "forge-std/console.sol";
 
 contract RumibaToken is ERC20, Ownable {
+    error ExceedsMaxSupply(uint256 requested, uint256 available);
+
     uint256 public immutable INITIAL_SUPPLY = 1_000_000e18; // 1 million tokens
     uint256 public immutable MAX_SUPPLY = 10_000_000e18; // 10 million tokens
 
-    constructor() ERC20("RumbaToken", "RUMBA") Ownable(msg.sender) {}
+    constructor() ERC20("RumbaToken", "RUMBA") Ownable(msg.sender) {
+        _mint(msg.sender, INITIAL_SUPPLY); // Mint 1 million tokens to the deployer
+    }
 
-    function mint() external onlyOwner {
-        _mint(msg.sender, INITIAL_SUPPLY); // Mint 1 million tokens to deployer
+    function mint(address to, uint256 amount) external onlyOwner {
+        if (totalSupply() + amount > MAX_SUPPLY) {
+            revert ExceedsMaxSupply({requested: amount, available: MAX_SUPPLY - totalSupply()});
+        }
+        _mint(to, amount);
     }
 }
